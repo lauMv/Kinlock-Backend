b84d59d539ea16f59610f4b599e9ca97454aab4d
lauMv <42174201+lauMv@users.noreply.github.com>
2025-09-08 00:22:52 -0400

    initial structure

---
+++ src/main/java/com/app/kinlock/KinlockApplication.java
@@ -0,0 +1,13 @@
+package com.app.kinlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/kinlock/KinlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.kinlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
