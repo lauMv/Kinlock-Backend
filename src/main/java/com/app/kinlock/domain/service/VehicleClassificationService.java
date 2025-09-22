0db74f979cad30f5fba04f583fc8725b32b9ddc6
lauMv <42174201+lauMv@users.noreply.github.com>
2025-09-22 00:29:11 -0400

    remove entity

--- src/main/java/com/app/kinlock/domain/service/VehicleClassificationService.java
+++
@@ -1,8 +0,0 @@
-package com.app.kinlock.domain.service;
-
-import com.app.kinlock.domain.entities.VehicleClassification;
-
-public interface VehicleClassificationService extends CRUDService<VehicleClassification, Integer> {
-
-    VehicleClassification create(VehicleClassificationDto dto);
-}
---
+++ src/main/java/com/app/kinlock/presentation/dto/VehicleDto.java
@@ -0,0 +1,4 @@
+package com.app.kinlock.presentation.dto;
+
+public class VehicleDto {
+}
