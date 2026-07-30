93e3f5cb67fa0c358a549f25aae34ad0aa2c875c
lauMv <42174201+lauMv@users.noreply.github.com>
2026-07-30 14:22:34 -0400

    update dto and pojo

--- src/main/java/com/app/kinlock/data/PlanRepository.java
+++ src/main/java/com/app/kinlock/data/PlanRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface PlanRepository extends GenericRepository<Plan, Integer> {
 
     @Query("SELECT new com.app.kinlock.presentation.pojo.PlanPojo ( " +
-            "p.id, p.active, p.vehicleCatalog.id, p.regional.id, p.insurance.id, p.minimumPremium, p.rate, p.ageLimit, p.discount, p.franchise, p.segment.name, p.planType.name, p.broker.name) " +
+            "p.id, p.active, p.vehicleCatalog.id, p.regional.id, p.insurance.id, p.minimumPremium, p.rate, p.ageLimit, p.discount, p.franchise, p.interest, p.segment.name, p.planType.name, p.broker.name) " +
             "FROM Plan p ")
     List<PlanPojo> getAllPojo();
 }
